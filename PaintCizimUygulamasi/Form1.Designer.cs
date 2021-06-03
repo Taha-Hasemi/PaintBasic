e2aa7de4af2d7f57e133454a644f5fc517f19698
Kizil Aslan <kiziltahaaslan@gmail.com>
2021-06-04 01:08:46 +0430

    first commit

---
+++ PaintCizimUygulamasi/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace PaintCizimUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.comboBoxEdit1 = new DevExpress.XtraEditors.ComboBoxEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(300, 372);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Renk Seç";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // comboBoxEdit1
+            // 
+            this.comboBoxEdit1.AllowDrop = true;
+            this.comboBoxEdit1.CausesValidation = false;
+            this.comboBoxEdit1.EditValue = "";
+            this.comboBoxEdit1.Location = new System.Drawing.Point(111, 374);
+            this.comboBoxEdit1.Name = "comboBoxEdit1";
+            this.comboBoxEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.comboBoxEdit1.Properties.Items.AddRange(new object[] {
+            "1",
+            "3",
+            "5",
+            "7",
+            "9",
+            "11",
+            "13",
+            "15",
+            "17",
+            "19",
+            "20"});
+            this.comboBoxEdit1.Size = new System.Drawing.Size(165, 20);
+            this.comboBoxEdit1.TabIndex = 1;
+            this.comboBoxEdit1.SelectedIndexChanged += new System.EventHandler(this.comboBoxEdit1_SelectedIndexChanged);
+            this.comboBoxEdit1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBoxEdit1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 423);
+            this.Controls.Add(this.comboBoxEdit1);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit1;
+    }
+}
+
---
+++ PaintCizimUygulamasi/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintCizimUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool ciz = false;
+        int baslax, baslay;
+        int kalinlik = 3;
+        ColorDialog colorDialog = new ColorDialog();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Pen pen = new Pen(colorDialog.Color, kalinlik);
+            Point point1 = new Point(baslax, baslay);
+            Point point2 = new Point(e.X, e.Y);
+            Graphics g = this.CreateGraphics();
+            if (ciz == true)
+            {
+                g.DrawLine(pen, point1, point2);
+                baslax = e.X;
+                baslay = e.Y;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            ciz = false;
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowDialog();
+        }
+
+        private void comboBoxEdit1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                try
+                {
+                    kalinlik = Convert.ToInt32(comboBoxEdit1.Text);
+                }
+                catch (Exception aciklama)
+                {
+                    comboBoxEdit1.Text = "";
+                    MessageBox.Show(aciklama.Message, "Sorun");
+                }
+            }
+        }
+
+        private void comboBoxEdit1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                kalinlik = Convert.ToInt32(comboBoxEdit1.Text);
+            }
+            catch (Exception aciklama)
+            {
+                comboBoxEdit1.Text = "";
+                MessageBox.Show(aciklama.Message, "Sorun");
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ciz = true;
+            baslax = e.X;
+            baslay = e.Y;
+        }
+    }
+}
